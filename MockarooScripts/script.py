f3c790c0048cce58f6fd12dd2cffc1576755714d
DorianLeci <dleci00@fesb.hr>
2025-12-04 19:52:34 +0100

    Add python script for inserting data from Mockaroo to DB.

---
+++ MockarooScripts/script.py
@@ -0,0 +1,76 @@
+import requests
+import psycopg2
+import random
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY=os.getenv("MOCKAROO_API_KEY")
+print(API_KEY)
+
+def Fetch(schema_id,count=1000):
+    url=f"https://api.mockaroo.com/api/{schema_id}?count={count}&key={API_KEY}"
+    response=requests.get(url)
+    return response.json()
+
+def GetData(cur,count=1000):   
+    CountryInsert(cur)
+    CityInsert(cur)
+    VisitorInsert(cur)
+
+
+
+
+
+def CountryInsert(cur,count=1000):
+    cur.execute("SELECT COUNT(*) FROM country")
+    if(cur.fetchone()[0]==0):
+        country_schema_id="8ca42db0"
+        country=Fetch(country_schema_id)
+
+        for c in country:
+            cur.execute("INSERT INTO public.country (country_id,name) VALUES (%s,%s)",(c["country_id"],c["name"]))       
+
+def CityInsert(cur,count=1000):
+    cur.execute("SELECT COUNT(*) FROM city")
+    if(cur.fetchone()[0]==0):   
+        city_schema_id="c27fc0b0"
+        cityData=Fetch(city_schema_id)
+
+        cur.execute("SELECT country_id FROM country")
+
+        country_id=[row[0] for row in cur.fetchall()]
+
+        for city in cityData:
+            city["country_id"]=random.choice(country_id)
+            cur.execute(f"INSERT INTO public.city (city_id,name,postal_code,country_id) VALUES (%s,%s,%s,%s)",(city["city_id"],city["name"],city["postal_code"],city["country_id"]))
+
+
+def VisitorInsert(cur,count=1000):
+    cur.execute("SELECT COUNT(*) FROM visitor")
+    if(cur.fetchone()[0]==0):   
+        visitor_schema_id="b2b8bd60"
+        visitorData=Fetch(visitor_schema_id)
+
+        cur.execute("SELECT city_id FROM city")
+
+        city_id=[row[0] for row in cur.fetchall()]
+
+        for visitor in visitorData:
+            visitor["city_id"]=random.choice(city_id)
+            cur.execute(f"INSERT INTO public.visitor (visitor_id,name,surname,email,birth_date,city_id) VALUES (%s,%s,%s,%s,%s,%s)",(visitor["visitor_id"],visitor["name"],
+            visitor["surname"],visitor["email"],visitor["birth_date"],visitor["city_id"]))
+
+
+conn=psycopg2.connect(host="localhost",dbname="Internship-5-FestivalDB",user="postgres",password="postgres")
+cur=conn.cursor()
+
+GetData(cur)
+conn.commit()
+
+cur.close()
+conn.close()
+
+
+
